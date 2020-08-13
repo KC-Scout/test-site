56fc9ee5c756af2538203d522380ed7768b59504
Connor <foster.clee@gmail.com>
2020-08-12 19:00:02 -0500

    set user name to display in heading, username persists across sessions using loclStorage

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,31 @@ let  colorButton = document.querySelector('#colorButton');
     document.querySelector('body').style.backgroundColor = color; 
    
 }
+
+
+let userButton = document.querySelector('#userButton');
+
+
+function setUserName() {
+    let myName = prompt('Please enter your name: ');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName)
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+    
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+userButton.onclick = function() {
+    setUserName();
+}
+
+
